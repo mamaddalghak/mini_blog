006582dbbf341e1c63a6f3db56d12128c2ab79d2
mamaddalghak <146006320+mamaddalghak@users.noreply.github.com>
2023-09-26 20:11:20 +0330

    Update mini_blog.py

--- mini_blog.py
+++ mini_blog.py
@@ -5,47 +5,111 @@ from comment import comment
 dicta = dict([])
 posts = []
 while True:
-    z = str(input())
+    tah = False
+    z = str(input()) + " "
     i = 0
     while z[i] ==" ":
         i = i + 1
     r = i
     if z[r:r + 2] == "sp":
-        i = i + 2
-        y = z[i:len(z)]
+        z = str(input()) + " "
+        i = 0
+        while z[i] == " ":
+            i = i + 1
+        r = i
+        while i < len(z):
+            if z[i] == " " and tah == False:
+                o = i
+                tah = True
+            elif z[i] != " ":
+                tah = False
+            i = i + 1
+        y = z[r + 2:o]
         print(dicta[y].posts)
     elif z[r:r + 2] == "sc":
-        i = i + 2
-        y = z[i:len(z)]
+        z = str(input()) + " "
+        i = 0
+        while z[i] == " ":
+            i = i + 1
+        r = i
+        while i < len(z):
+            if z[i] == " " and tah == False:
+                o = i
+                tah = True
+            elif z[i] != " ":
+                tah = False
+            i = i + 1
+        y = z[r + 2:o]
         for j in posts:
             if j.text == y:
                 print(j.comments)
     elif z[r:r + 2] == "p:":
-        i = i + 2
-        o = i
+        while i < len(z):
+            if z[i] == " " and tah == False:
+                o = i
+                tah = True
+            elif z[i] != " ":
+                tah = False
+            i = i + 1
+        x = z[r + 2:o]
+        z = str(input()) + " "
+        i = 0
         while z[i:i + 2] != "n:":
             i = i + 1
-        x = z[o:i]
-        y = z[i + 2:len(z)]
+        r = i
+        tah = False
+        while i < len(z):
+            if z[i] == " " and tah == False:
+                o = i
+                tah = True
+            elif z[i] != " ":
+                tah = False
+            i = i + 1
+        y = z[r + 2:o]
         if y not in dicta.keys():
+            print(x)
             dicta[y] = human(y, [x])
         else:
+            print(x)
             dicta[y].posts.append(x)
         posts.append(post(x, y, {}))
     elif z[r:r + 2] == "l:":
         pass
     elif z[r:r + 2] == "c:":
-        i = i + 2
-        o = i
-        while z[i:i + 2] != "n:":
+        while i < len(z):
+            if z[i] == " " and tah == False:
+                o = i
+                tah = True
+            elif z[i] != " ":
+                tah = False
             i = i + 1
-        c = z[o:i]
-        i = i + 2
-        o = i
-        while z[i:i + 2] != "p:":
+        c = z[r + 2:o]
+        z = str(input()) + " "
+        i = 0
+        while z[i] == " ":
             i = i + 1
-        y = z[o:i]
-        x = z[i + 2:len(z)]
+        r = i
+        while i < len(z):
+            if z[i] == " " and tah == False:
+                o = i
+                tah = True
+            elif z[i] != " ":
+                tah = False
+            i = i + 1
+        y = z[r + 2:o]
+        z = str(input()) + " "
+        i = 0
+        while z[i] == " ":
+            i = i + 1
+        r = i
+        while i < len(z):
+            if z[i] == " " and tah == False:
+                o = i
+                tah = True
+            elif z[i] != " ":
+                tah = False
+            i = i + 1
+        x = z[r + 2:o]
         for j in posts:
             if j.text == x:
                 j.comments[y] = c
