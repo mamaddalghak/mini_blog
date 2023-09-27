9fc072a24991446e89100d2da0e0ad41b40a3674
mamaddalghak <146006320+mamaddalghak@users.noreply.github.com>
2023-09-27 20:18:26 +0330

    Add files via upload

---
+++ show_comments.py
@@ -0,0 +1,15 @@
+def show_comments(your_str, posts):
+    tah = False
+    cursor = 0
+    while your_str[cursor] == " ":
+        cursor = cursor + 1
+    cursor2 = cursor
+    for cursor in range(cursor, len(your_str)):
+        if your_str[cursor] == " " and tah == False:
+            cursor3 = cursor
+            tah = True
+        elif your_str[cursor] != " ":
+            tah = False
+        cursor = cursor + 1
+    post = int(your_str[cursor2 + 2:cursor3])
+    print(posts[post - 1].comments)
